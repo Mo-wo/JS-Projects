4ef159b7eb61f0227f8f089084bb8ddf56ca37a4
mowo <toryvick20@gmail.com>
2022-02-11 13:37:03 +0100

    Background color changed on button click

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-window.addEventListener ("click", function(){
-    function getColor() {
-        return "#" + Math.floor(Math.random());
-    }
-    console.log(getColor());
-
+window.addEventListener ("load", (event) => {
+    let btn = document.getElementsByTagName("button")[0];
+    let body = document.getElementsByTagName("body")[0];
+    
+    function getRandom() {
+        return "#"+ Math.random().toString(16).slice(2,8);      
+    };
+    btn.addEventListener('click', (event) => {
+        function changeColor(){
+            let newColor = getRandom();
+            body.style.backgroundColor = newColor;
+            
+            let text = document.getElementsByTagName("h1")[0];
+            text.textContent = `This color is: ${newColor}`
+        }
+        changeColor();
+    });
 });
